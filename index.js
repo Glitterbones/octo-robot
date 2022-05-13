223a92fa6bffa217f1ffd3097f74b0af605a1da7
ashley lockhart <allockhart49@gmail.com>
2022-05-13 14:17:09 -0600

    npms and html

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const Manager = require('./lib/Manager.js');
 const Engineer = require('./lib/Engineer.js');
 const Intern = require('./lib/Intern.js');
 const fs = require('fs');
-
+const createHTML = require('./src/page-template.js');
 let teamArray = [];
 
 const createManager = () => {
